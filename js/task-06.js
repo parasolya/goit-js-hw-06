c32301de952479a6c5336f8bd5ee41577bfb1d47
Olga Pysarenko <ola.shevchuk@gmail.com>
2023-02-27 22:15:06 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,13 @@
 const inputEl = document.querySelector('#validation-input');
 
-inputEl.addEventListener("blur", (event) => {  
-    inputEl.classList.add('invalid');      
+inputEl.addEventListener("blur", (event) => {           
     if (event.currentTarget.value.length === Number(event.currentTarget.dataset.length)) {
-        inputEl.classList.replace('invalid', 'valid');
-    }    
-     
+        inputEl.classList.add('valid');
+        inputEl.classList.remove('invalid');
+    } else {
+        inputEl.classList.add('invalid');
+        inputEl.classList.remove('valid');
+      }    
     });
     
     
